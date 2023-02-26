c13426bd20f62a48bab31e9d84c416411ac7af5b
Vee339 <81091817+Vee339@users.noreply.github.com>
2023-02-26 15:52:33 +0530

    this is the first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const menuBtn = document.querySelector(".menuBtn");
+const menu = document.querySelector(".menu");
+
+window.addEventListener("scroll", () => {
+  const row1 = document.querySelector(".row1");
+  const row2 = document.querySelector(".row2");
+  const row3 = document.querySelector(".row3");
+  const row4 = document.querySelector(".row4");
+
+  const row1Position = row1.getBoundingClientRect().top;
+  const row2Position = row2.getBoundingClientRect().top;
+  const row3Position = row3.getBoundingClientRect().top;
+  const row4Position = row4.getBoundingClientRect().top;
+
+  if (row1Position < window.innerHeight) {
+    row1.classList.add("animate");
+    document.querySelector(".imgBox.second").classList.add("animate");
+    document.querySelector(".imgBox.third").classList.add("animate");
+  }
+  if (row2Position < window.innerHeight) {
+    row2.classList.add("animate");
+  }
+  if (row3Position < window.innerHeight) {
+    row3.classList.add("animate");
+    document.querySelector(".imgBox.sixth").classList.add("animate");
+  }
+  if (row4Position < window.innerHeight) {
+    row4.classList.add("animate");
+    document.querySelector(".imgBox.eighth").classList.add("animate");
+  }
+});
+
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("open");
+  menu.classList.toggle("open");
+});
